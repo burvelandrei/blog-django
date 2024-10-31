8a75efa8438f1bf3385bc84e18be9164e63cabdb
Andrei Burvel <burvelandrei@gmail.com>
2024-11-01 02:02:18 +0300

    V 1.1 добавил представления и модели

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from publication.models import Publication
 
-# Create your models here.
+
+class Comment(models.Model):
+    publication = models.ForeignKey(
+        Publication, on_delete=models.CASCADE, related_name="comments"
+    )
+    content = models.TextField()
+
+    def __str__(self):
+        return f"Comment on {self.publication.title}"
