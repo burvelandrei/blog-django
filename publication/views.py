29111483a25e97b407836b5396dd78befeb48993
Andrei Burvel <burvelandrei@gmail.com>
2024-11-01 02:40:25 +0300

    V 1.2 добавил ссылку

--- publication/views.py
+++ publication/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Publication
+
 
 def index(request):
-    return render(request, 'publication/index.html')
+    publications = Publication.objects.all()
+    return render(request, "publication/index.html", {"publications": publications})
+
+
+def publication_detail(request, id):
+    publication = get_object_or_404(Publication, id=id)
+    return render(
+        request, "publication/publication_detail.html", {"publication": publication}
+    )
