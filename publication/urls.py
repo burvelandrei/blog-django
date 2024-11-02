51cf04f36f3e29f865df05ccbd9d8cbd491ea829
Andrei Burvel <burvelandrei@gmail.com>
2024-11-02 21:36:37 +0300

    V 1.8 добавил urls

--- publication/urls.py
+++ publication/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('publication/<int:id>', views.publication_detail, name='publication_detail')
+    path('publication/<int:id>', views.publication_detail, name='publication_detail'),
+    path('publication/add', views.add_publication, name="add_publication"),
+    path('publication/<int:id>/delete', views.delete_publication, name="delete_publication"),
+    path('publication/<int:id>/comments/', views.add_comment, name='add_comment')
 ]
