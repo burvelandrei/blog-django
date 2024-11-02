809cbb095737abf3d3362b38d4216f287f554355
Andrei Burvel <burvelandrei@gmail.com>
2024-11-02 21:35:27 +0300

    V 1.5 обновил модель comment, добавил created_at

--- comment/models.py
+++ comment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from publication.models import Publication
+from django.utils import timezone
 
 
 class Comment(models.Model):
@@ -7,6 +8,7 @@ class Comment(models.Model):
         Publication, on_delete=models.CASCADE, related_name="comments"
     )
     content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"Comment on {self.publication.title}"
