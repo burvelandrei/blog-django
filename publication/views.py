24807b174458defe3a89c1090bdbcb624e1a786e
Andrei Burvel <burvelandrei@gmail.com>
2024-11-02 21:34:44 +0300

    V 1.4 добавил templstes

--- publication/views.py
+++ publication/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Publication
+from .forms import PublicationForm
+from comment.models import Comment
+from comment.forms import CommentForm
 
 
 def index(request):
@@ -9,6 +12,38 @@ def index(request):
 
 def publication_detail(request, id):
     publication = get_object_or_404(Publication, id=id)
+    comments = Comment.objects.filter(publication=id)
     return render(
-        request, "publication/publication_detail.html", {"publication": publication}
+        request,
+        "publication/publication_detail.html",
+        {"publication": publication, "comments": comments},
     )
+
+def add_publication(request):
+    if request.method == "GET":
+        form = PublicationForm()
+        return render(request, 'publication/add_publication.html', {'form': form})
+    elif request.method == 'POST':
+        form = PublicationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        else:
+            form = PublicationForm()
+        return render(request, 'publication/add_publication.html', {'form': form})
+
+def delete_publication(request, id):
+    publication = Publication.objects.filter(id=id)
+    publication.delete()
+    return redirect("index")
+
+def add_comment(request, id):
+    publication = get_object_or_404(Publication, id=id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.publication = publication
+            comment.save()
+            return redirect('publication_detail', id=id)
