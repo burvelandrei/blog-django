4d333d7c91a7491d63b74767768c79d5909700d4
Andrei Burvel <burvelandrei@gmail.com>
2024-11-13 11:43:09 +0300

    V 1.10 добавил регистрацию

---
+++ users/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import RegistrationForm, LoginForm
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            # Если форма невалидна, передаем ее обратно в шаблон с ошибками
+            return render(request, 'users/register.html', {'form': form})
+    else:
+        form = RegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = LoginForm()
+    return render(request, 'users/login.html', {'form': form})
