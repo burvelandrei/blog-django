000f933b3278d7f5bacfd8a1ac3720690104fbd4
Andrei Burvel <burvelandrei@gmail.com>
2024-11-16 14:10:16 +0300

    V 1.22 расширена админка для всех app

--- publication/admin.py
+++ publication/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
 from .models import Publication
 
-admin.site.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    list_filter = ('author', 'created_at')
+    search_fields = ('title', 'author__username')
+    ordering = ('-created_at',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'content', 'author')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('created_at', 'updated_at'),
+        }),
+    )
+    readonly_fields = ('created_at', 'updated_at')
+
+admin.site.register(Publication, PublicationAdmin)
